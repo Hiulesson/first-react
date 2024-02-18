120d07336e331b4dd189ad8d2fc80802b58e2a9b
Hiulesson Renan <hiulessorenan@gmail.com>
2024-02-18 11:53:55 -0300

    Modificando: TaskList -> Botao 'adicionar' implementado

--- src/TaskList/TaskList.tsx
+++ src/TaskList/TaskList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import TaskItem from "../TaskItem/TaskItem";
 
 // Definindo o tipo para as props
@@ -8,15 +8,34 @@ interface TaskListProps {
 
 // Componente TaskList
 const TaskList: React.FC<TaskListProps> = ({ tasks }) => {
+
+  const [newTask, setNewTask] = useState<string>('');
+  const [taskList, setTaskList] = useState<string[]>(tasks);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      setTaskList([...taskList, newTask]); // Adiciona a nova tarefa à lista de tarefas
+      setNewTask(''); // Limpa o campo de entrada de texto
+    }
+  };
+
   return (
     <div>
       <h2>Lista de Tarefas</h2>
       <ul>
         {/* Mapeia as tarefas recebidas como props e renderiza cada uma delas usando o componente TaskItem */}
-        {tasks.map((task, index) => (
+        {taskList.map((task, index) => (
           <TaskItem key={index} task={task} />
         ))}
       </ul>
+      <div>
+        <input type="text" value={newTask} onChange={handleInputChange} />
+        <button onClick={handleAddTask}>Adicionar</button>
+      </div>
     </div>
   );
 };
